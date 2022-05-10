2bef8cf8399078c2ed1d9192378d6135c8d6b572
Codrin Sirboiu <codrin.sirboiu@gmail.com>
2022-05-10 15:57:13 +0100

    Add accept invitation functionality

--- app/dot/cpp/login/controllers/UserController.java
+++ app/dot/cpp/login/controllers/UserController.java
@@ -5,13 +5,17 @@ import dot.cpp.login.exceptions.LoginException;
 import dot.cpp.login.helpers.CookieHelper;
 import dot.cpp.login.models.user.entity.User;
 import dot.cpp.login.models.user.repository.UserRepository;
+import dot.cpp.login.models.user.request.SetPasswordRequest;
 import dot.cpp.login.service.LoginService;
 import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import play.data.FormFactory;
+import play.i18n.MessagesApi;
 import play.libs.mailer.Email;
 import play.libs.mailer.MailerClient;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Http.Request;
 import play.mvc.Result;
 
@@ -20,15 +24,22 @@ public class UserController extends Controller {
   private static final Class<User> clazz = User.class;
 
   private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final FormFactory formFactory;
+  private final MessagesApi messagesApi;
+
   private final UserRepository userRepository;
   private final LoginService loginService;
 
   private final MailerClient mailerClient;
 
   @Inject
-  public UserController(UserRepository user, LoginService loginService, MailerClient mailerClient) {
+  public UserController(UserRepository user, LoginService loginService,
+                        FormFactory formFactory, MessagesApi messagesApi,
+                        MailerClient mailerClient) {
     this.userRepository = user;
     this.loginService = loginService;
+    this.formFactory = formFactory;
+    this.messagesApi = messagesApi;
     this.mailerClient = mailerClient;
   }
 
@@ -90,5 +101,71 @@ public class UserController extends Controller {
 
   public Result get(String id) {
     return ok(userRepository.findById(id, clazz).toString());
+  }
+
+  /**
+   * Activate account page for admin.
+   *
+   * @param request implicit request, must contain body with password and repeat password
+   * @return accept invitation page
+   */
+  public Result acceptInvitationPage(Http.Request request, String resetPasswordUuid) {
+
+    final var messages = messagesApi.preferred(request);
+    final var form = formFactory.form(SetPasswordRequest.class);
+    final var repoCtx = request.attrs().get(ForKidsAttributes.REPO_CONTEXT);
+
+    if (!userProfileManager.resetPasswordUuidIsValid(
+            repoCtx, resetPasswordUuid, ForKidsEntityType.ADMIN)) {
+      return redirect(getLoginPage())
+              .flashing("alert-danger", messages.apply("userProfile.passwordUuid.alreadyUsed"));
+    }
+
+    return ok(
+            dot.cpp.login.views.html.setPassword.render(
+                    form, resetPasswordUuid, request, messages));
+  }
+
+  /**
+   * Activates admin account.
+   *
+   * @param request Request
+   * @return accept invitation
+   */
+  public Result acceptInvitation(Http.Request request, String resetPasswordUuid) {
+    logger.debug("START");
+    final var messages = messagesApi.preferred(request);
+    final var form = formFactory.form(SetPasswordRequest.class).bindFromRequest(request);
+
+    if (form.hasErrors()) {
+      return requestErrorHandlerService.handleFormErrorWithRefresh(request, form);
+    }
+
+    var setPasswordRequest = form.get();
+    logger.debug("setPasswordRequest: {}", setPasswordRequest);
+
+    try {
+      final var dbAdminProfile =
+              userProfileManager.acceptInvitation(
+                      setPasswordRequest, resetPasswordUuid, ForKidsEntityType.ADMIN);
+
+      final var session =
+              userProfileManager.login(
+                      request,
+                      dbAdminProfile.getEmail(),
+                      setPasswordRequest.getPassword(),
+                      UserProfileRole.ADMIN,
+                      ForKidsEntityType.ADMIN,
+                      ForKidsApplication.ADMIN);
+
+      return getSuccessfulResult(
+              routes.AwpDashboardController.dashboard(),
+              messages.apply("userProfile.newPasswordSuccess"))
+              .withCookies(
+                      CookieHelper.getAccessCookie(session, ForKidsApplication.ADMIN),
+                      CookieHelper.getRefreshCookie(session, ForKidsApplication.ADMIN));
+    } catch (ForKidsComponentException | AuthenticationException e) {
+      return requestErrorHandlerService.handleXaftException(getLoginPage(), request, e);
+    }
   }
 }
---
+++ app/dot/cpp/login/models/user/request/SetPasswordRequest.java
@@ -0,0 +1,49 @@
+package dot.cpp.login.models.user.request;
+
+import com.google.gson.Gson;
+import dot.cpp.repository.constants.Patterns;
+import play.data.validation.Constraints;
+import play.data.validation.Constraints.Pattern;
+import play.data.validation.Constraints.Validatable;
+import play.data.validation.Constraints.Validate;
+
+@Validate
+public class SetPasswordRequest implements Validatable<String> {
+
+  @Pattern(value = Patterns.ALPHA_PASS_MIN8, message = "constraints.field.invalid")
+  @Constraints.MinLength(value = 1, message = "constraints.field.invalid")
+  private String password;
+
+  @Pattern(value = Patterns.ALPHA_PASS_MIN8, message = "constraints.field.invalid")
+  @Constraints.MinLength(value = 1, message = "constraints.field.invalid")
+  private String confirmPassword;
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getConfirmPassword() {
+    return confirmPassword;
+  }
+
+  public void setConfirmPassword(String confirmPassword) {
+    this.confirmPassword = confirmPassword;
+  }
+
+  @Override
+  public String validate() {
+    if (!password.equals(confirmPassword)) {
+      return "general.passwords.not.match";
+    }
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return new Gson().toJson(this);
+  }
+}
